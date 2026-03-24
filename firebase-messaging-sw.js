0524c5a8edf8e712db9b43a4e2aa9a5b99b48eb4
ricobetmarketing <marketing@rico.bet>
2026-03-24 17:25:25 +0800

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,10 +2,10 @@ importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js'
 importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey: "YOUR_API_KEY",
-  projectId: "YOUR_PROJECT_ID",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBgV0PcWtjr2R1gAeIR0Obzo_Uufo34K3E",
+  projectId: "ai-live-chat-d30d7",
+  messagingSenderId: "330505566191",
+  appId: "1:330505566191:web:1cb7244d16656d6a05a81a"
 });
 
 const messaging = firebase.messaging();
